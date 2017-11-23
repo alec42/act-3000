9c0eefac0600826f0e62cefeb55eefed626bf5db
Christopher Desktop <christopher.blierwong@gmail.com>
2017-11-23 16:54:25 -0500

    ajout des fichiers pour la semaine 12

---
+++ depannage/semaine-12.R
@@ -0,0 +1,13 @@
+# Code R presente lors du 24 novembre 2017
+# Christopher Blier-Wong
+# ACT-3000 Theorie du risque
+
+# Plan du depannage -------------------------------------------------------
+
+# Méthode des rectangles
+# Estimation 
+
+
+
+# Méthode des rectangles --------------------------------------------------
+
